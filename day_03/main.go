744c66941fb1c731468bda83f7d1d19a20371003
Asgeir Melling <asgmel@gmail.com>
2025-12-03 08:24:46 +0100

    feat: finish day 3, task one

--- day_03/main.go
+++ day_03/main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+
+	"github.com/asgmel/aockit/input"
 )
 
 func main() {
-	fmt.Println("Day 03")
+	taskOne("./input.txt")
+}
+
+func taskOne(inputPath string) {
+	puzzleInput := input.ReadInputLines(inputPath)
+	totalJoltage := 0
+	for _, bank := range puzzleInput {
+		totalJoltage += findMaxJoltageInBank(bank)
+	}
+	fmt.Printf("Total joltage: %d\n", totalJoltage)
+}
+
+func findMaxJoltageInBank(bank string) (joltage int) {
+	idx, firstJoltage := findMaxJoltage(bank[:len(bank)-1])
+	_, secondJoltage := findMaxJoltage(bank[idx+1:])
+	fmt.Printf("Bank %s: max joltages are %d and %d\n", bank, firstJoltage, secondJoltage)
+	result, err := strconv.Atoi(string(firstJoltage) + string(secondJoltage))
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func findMaxJoltage(bank string) (index int, joltage rune) {
+	for idx, jolt := range bank {
+		if jolt > joltage {
+			joltage = jolt
+			index = idx
+		}
+	}
+	return index, joltage
 }
