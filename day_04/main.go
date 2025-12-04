18f9cace0ccba1f705681635c8148a577f4cad70
Asgeir Melling <asgmel@gmail.com>
2025-12-04 08:53:37 +0100

    feat: finish day 4

--- day_04/main.go
+++ day_04/main.go
@@ -2,8 +2,95 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/asgmel/aockit/grid"
+	"github.com/asgmel/aockit/input"
 )
 
 func main() {
-	fmt.Println("Day 04")
+	taskOne("./input.txt")
+	taskTwo("./input.txt")
+}
+
+func taskOne(inputPath string) {
+	puzzleInput := input.ReadInputLetters(inputPath)
+	markedInput := markForkliftAccessablePaper(puzzleInput)
+	accessablePaperCount := countRemovablePaper(markedInput)
+	println("Accessable paper count:", accessablePaperCount)
+}
+
+func taskTwo(inputPath string) {
+	puzzleInput := input.ReadInputLetters(inputPath)
+	count := recursiveCountMarkedPaper(puzzleInput)
+	println("Accessable paper count:", count)
+}
+
+func recursiveCountMarkedPaper(matrix [][]string) int {
+	count := 0
+	markedMatrix := markForkliftAccessablePaper(matrix)
+	count += countRemovablePaper(markedMatrix)
+	newMatrix := removeMarkedPaper(markedMatrix)
+
+	if countRemovablePaper(markedMatrix) > 0 {
+		count += recursiveCountMarkedPaper(newMatrix)
+	}
+	return count
+}
+
+func countRemovablePaper(matrix [][]string) (count int) {
+	for y, row := range matrix {
+		for x := range row {
+			if matrix[y][x] == "x" {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func removeMarkedPaper(matrix [][]string) [][]string {
+	removedMatrix := make([][]string, len(matrix))
+	for i := range matrix {
+		removedMatrix[i] = make([]string, len(matrix[i]))
+		copy(removedMatrix[i], matrix[i])
+	}
+	for y, row := range matrix {
+		for x := range row {
+			if matrix[y][x] == "x" {
+				removedMatrix[y][x] = "."
+			}
+		}
+	}
+	return removedMatrix
+}
+
+func markForkliftAccessablePaper(matrix [][]string) [][]string {
+	markedMatrix := make([][]string, len(matrix))
+	for i := range matrix {
+		markedMatrix[i] = make([]string, len(matrix[i]))
+		copy(markedMatrix[i], matrix[i])
+	}
+	for y, row := range matrix {
+		for x := range row {
+			position := grid.Position{X: x, Y: y}
+			neighbourCount := countNeighboursWithPaper(position, matrix)
+			if neighbourCount < 4 && matrix[y][x] == "@" {
+				markedMatrix[y][x] = "x"
+			}
+		}
+	}
+	for _, row := range markedMatrix {
+		fmt.Printf("%s\n", row)
+	}
+	return markedMatrix
+}
+
+func countNeighboursWithPaper(position grid.Position, matrix [][]string) (count int) {
+	neighbours := grid.GetNeighbouringPositions(position, matrix)
+	for _, neighbour := range neighbours {
+		if matrix[neighbour.Y][neighbour.X] == "@" {
+			count++
+		}
+	}
+	return count
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/asgmel/aoc2025
 
 go 1.25.4
 
-require github.com/asgmel/aockit v0.0.0-20251130173616-e8acb914d9eb
+require github.com/asgmel/aockit v0.0.0-20251204070829-69e194cfd5a1
