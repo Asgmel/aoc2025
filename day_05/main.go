eae55f873449a4b479916ace1693002cc545cc44
Asgeir Melling <asgmel@gmail.com>
2025-12-05 11:48:39 +0100

    feat: finish day 5 part one

--- day_05/main.go
+++ day_05/main.go
@@ -2,8 +2,66 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/asgmel/aockit/input"
 )
 
+type freshRange struct {
+	start int
+	end   int
+}
+
+func (r freshRange) InRange(val int) bool {
+	return val >= r.start && val <= r.end
+}
+
 func main() {
-	fmt.Println("Day 05")
+	taskOne("./input.txt")
+}
+
+func taskOne(inputPath string) {
+	puzzleInput := input.ReadInputLines(inputPath)
+	freshIds, ids := formatPuzzleInput(puzzleInput)
+	count := countFreshIds(freshIds, ids)
+	fmt.Printf("Fresh id count: %d\n", count)
+}
+
+func countFreshIds(freshIds []freshRange, ids []int) (count int) {
+	for _, id := range ids {
+		if checkIdFreshness(freshIds, id) {
+			count++
+		}
+	}
+	return count
+}
+
+func checkIdFreshness(freshIds []freshRange, id int) bool {
+	for _, r := range freshIds {
+		if r.InRange(id) {
+			return true
+		}
+	}
+	return false
+}
+
+func formatPuzzleInput(input []string) (freshIds []freshRange, ids []int) {
+	readingIds := false
+	for _, line := range input {
+		if line == "" {
+			readingIds = true
+			continue
+		}
+		if readingIds {
+			id, _ := strconv.Atoi(line)
+			ids = append(ids, id)
+		} else {
+			splitLine := strings.Split(line, "-")
+			start, _ := strconv.Atoi(splitLine[0])
+			end, _ := strconv.Atoi(splitLine[1])
+			freshIds = append(freshIds, freshRange{start: start, end: end})
+		}
+	}
+	return freshIds, ids
 }
